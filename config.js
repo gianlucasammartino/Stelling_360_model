b4225ea4e92275abd551b63d96749575c0e4d207
gianlucasammartino <156948255+gianlucasammartino@users.noreply.github.com>
2024-01-17 19:37:51 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Tuesday 16th of January 2024 10:48:21 PM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'http://localhost/mytour',//path to the index.html
+    theme : 'theasys',
+};
